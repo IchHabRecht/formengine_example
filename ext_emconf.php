11cdfc64d2dabd57034c879e5c867a9f3b44a9ae
Nicole Cordes <cordes@cps-it.de>
2015-11-16 18:13:09 +0100

    [TASK] Add basic extension files
    Add ext_emconf.php and ext_icon.gif files

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "formengine_example".
+ *
+ * Auto generated 16-11-2015 18:12
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'FormEngine Example Extension',
+  'description' => 'This is an example extension to introduce the development with the new FormEngine',
+  'category' => 'backend',
+  'author' => 'Nicole Cordes',
+  'author_email' => 'typo3@cordes.co',
+  'author_company' => '',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'version' => '0.1.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.6.0-7.99.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:1:{s:12:"ext_icon.gif";s:4:"df08";}',
+);
+
