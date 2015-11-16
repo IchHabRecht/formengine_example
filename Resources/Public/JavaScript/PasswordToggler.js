23aecbdb38bf130ef14453ee225ac42ddd9a55a3
Nicole Cordes <cordes@cps-it.de>
2015-11-16 23:34:12 +0100

    [TASK] Add RequireJs functionality
    This JavaScript removes any registered action for onblur and onfocus of
    the form fields. Furthermore it adds an own function to stop the event
    from bubbling up the DOM tree.
    
    It adds the approbate HTML to show the button and registers and onclick
    function to switch the form field type.

--- Resources/Public/JavaScript/PasswordToggler.js
+++ Resources/Public/JavaScript/PasswordToggler.js
@@ -8,6 +8,36 @@ define(['jquery'], function($) {
 	var PasswordToggler = {
 
 		initialize: function() {
+			$('input.toggle-password-field').each(function() {
+				var $this = $(this);
+
+				$this
+				.off('blur focus')
+				.on('blur focus', function(event) {
+					event.stopImmediatePropagation();
+				})
+				.parent('div.input-group')
+				.append(
+					$('<span class="input-group-btn">')
+					.append(
+						$('<label class="btn btn-default">')
+						.attr('for', $this.attr('id'))
+						.text('SHOW')
+						.on('click', function() {
+							var $this = $(this),
+								$input = $('input#' + $this.attr('for'));
+
+							if ($input.attr('type') === 'password') {
+								$input.attr('type', 'text');
+								$this.text('HIDE');
+							} else {
+								$input.attr('type', 'password');
+								$this.text('SHOW');
+							}
+						})
+					)
+				)
+			});
 		}
 	};
 
