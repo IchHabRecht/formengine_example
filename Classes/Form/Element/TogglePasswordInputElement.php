a546c7d6fe99af8852f7d373dba981986727c9bf
Nicole Cordes <cordes@cps-it.de>
2015-11-16 18:22:25 +0100

    [TASK] Add basis TogglePasswordInputElement class
    The class has to extend AbstractFormElement and must implement the
    public method render

---
+++ Classes/Form/Element/TogglePasswordInputElement.php
@@ -0,0 +1,46 @@
+<?php
+namespace IchHabRecht\FormengineExample\Form\Element;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2015 Nicole Cordes <typo3@cordes.co>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use TYPO3\CMS\Backend\Form\Element\AbstractFormElement;
+
+class TogglePasswordInputElement extends AbstractFormElement
+{
+
+    /**
+     * This will render a single-line input password field
+     * and a button to toggle password visibility
+     *
+     * @return array As defined in initializeResultArray() of AbstractNode
+     */
+    public function render()
+    {
+        $resultArray = $this->initializeResultArray();
+
+        return $resultArray;
+    }
+
+}
