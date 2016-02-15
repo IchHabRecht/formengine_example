15ca4bcd39501bbd03f77ccc31208c80d709b473
Christian Kuhn <lolli@schwarzbu.ch>
2016-02-15 16:09:21 +0100

    [TASK] Use locallang ajax loading api from core and fix inline loading

--- Resources/Public/JavaScript/PasswordToggler.js
+++ Resources/Public/JavaScript/PasswordToggler.js
@@ -16,23 +16,23 @@ define(['jquery'], function($) {
 				.on('blur focus', function(event) {
 					event.stopImmediatePropagation();
 				})
-				.parent('div.input-group')
+				.parent('div.input-group:not(:has(span.input-group-btn))')
 				.append(
 					$('<span class="input-group-btn">')
 					.append(
 						$('<label class="btn btn-default">')
 						.attr('for', $this.attr('id'))
-						.text(FormengineExample.lang['show'])
+						.text(TYPO3.lang['show'])
 						.on('click', function() {
 							var $this = $(this),
 								$input = $('input#' + $this.attr('for'));
 
 							if ($input.attr('type') === 'password') {
 								$input.attr('type', 'text');
-								$this.text(FormengineExample.lang['hide']);
+								$this.text(TYPO3.lang['hide']);
 							} else {
 								$input.attr('type', 'password');
-								$this.text(FormengineExample.lang['show']);
+								$this.text(TYPO3.lang['show']);
 							}
 						})
 					)
@@ -40,8 +40,6 @@ define(['jquery'], function($) {
 			});
 		}
 	};
-
-	$(PasswordToggler.initialize);
 
 	return PasswordToggler;
 });
